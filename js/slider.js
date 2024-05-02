2adbb29cb84e2ac1fb76fb9544a6e3e2c88aa3f4
Felipe Ramirez <dev.frmz08@gmail.com>
2024-05-02 01:48:37 -0600

    add slider

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 15,
+            stretch: 0,
+            depth: 300,
+            slideShadows: true,
+        },
+        loop: true
+    });
+});
